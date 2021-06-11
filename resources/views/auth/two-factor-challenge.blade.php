979183d49a8e82c0c6f10a38e034932685aa3334
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-11 18:07:33 +0300

    İki faktörlü kimlik doğrulama kurtarma kodları eklendi

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -39,6 +39,41 @@
                         </form>
                     </div>
                 </div>
+                <div class="card mt-3">
+                    <div class="card-header">{{ __('Two Factor Recovery Code') }}</div>
+
+                    <div class="card-body">
+                        <p class="text-center">
+                            {{ __('Please enter your authentication code to login.') }}
+                        </p>
+
+                        <form method="POST" action="{{ route('two-factor.login') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="recovery_code" class="col-md-4 col-form-label text-md-right">{{ __('Recovery Code') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="recovery_code" type="password" class="form-control @error('recovery_code') is-invalid @enderror" name="recovery_code" required>
+
+                                    @error('recovery_code')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Submit') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,6 +42,14 @@
                                 <div class="mb-3">
                                     {!! auth()->user()->twoFactorQrCodeSvg() !!}
                                 </div>
+                                <div>
+                                    <h3>Recovery Codes :</h3>
+                                    <ul>
+                                        @foreach(json_decode(decrypt(auth()->user()->two_factor_recovery_codes)) as $code)
+                                            <li>{{ $code }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
                                 <button class="btn btn-danger">Disable</button>
                             @else
                                 <button class="btn btn-primary">Enable</button>
