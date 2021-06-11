941935ed0710763b29f3ce9ca06de25245db9188
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-11 16:18:55 +0300

    İki faktörlü kimlik doğrulama için aktif ve pasif butonları oluşturuldu diğer gerekli işlemler yapıldı

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,35 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-8 mt-3">
+                <div class="card">
+                    <div class="card-header">{{ __('Two Factor Authentication') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status')=='two-factor-authentication-disabled')
+                            <div class="alert alert-success" role="alert">
+                                Two factor authentication has been disabled.
+                            </div>
+                        @endif
+
+                        @if (session('status')=='two-factor-authentication-enabled')
+                            <div class="alert alert-success" role="alert">
+                                Two factor authentication has been enabled.
+                            </div>
+                        @endif
+
+                        <form action="/user/two-factor-authentication" method="POST">
+                            @csrf
+                            @if(auth()->user()->two_factor_secret)
+                                @method('DELETE')
+                                <button class="btn btn-danger">Disable</button>
+                            @else
+                                <button class="btn btn-primary">Enable</button>
+                            @endif
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
